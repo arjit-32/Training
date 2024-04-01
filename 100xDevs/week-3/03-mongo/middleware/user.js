aeb8e268e4e10e974356bb3a7e41d340bbc44a32
Arjit Sharma <arjit_sharma@canalys.com>
2024-04-02 00:28:46 +0530

    w3 mongo assignment done

--- 100xDevs/week-3/03-mongo/middleware/user.js
+++ 100xDevs/week-3/03-mongo/middleware/user.js
@@ -1,6 +1,20 @@
+const { User } = require("../db");
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    User.exists({username: username, password: password})
+        .then(result => {
+            if(result){
+                next();
+            }else{
+                res.send("User doesnt exists")
+            }
+        })
+        .catch(err => "Some error occurred" );
 }
 
 module.exports = userMiddleware;
--- 100xDevs/week-3/03-mongo/routes/admin.js
+++ 100xDevs/week-3/03-mongo/routes/admin.js
@@ -1,18 +1,48 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const db = require("../db/index");
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    const adminDetails = new db.Admin({
+        username: username,
+        password: password
+    })
+
+    adminDetails.save()
+    .then(data => res.send("Admin created successfully"))
+    .catch(err => res.send("Error creating a Admin"));
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const {id, title, description, price, imageLink} = req.body;
+
+    const courseDetails = new db.Course({
+        id: id,
+        title: title,
+        description: description,
+        price: price,
+        imageLink: imageLink  
+    })
+
+    courseDetails.save()
+        .then(data => res.send("Course created successfully"))
+        .catch(err => res.send("Error creating course"))
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware,async (req, res) => {
     // Implement fetching all courses logic
+    const allCourses = await db.Course.find({});
+    console.log(allCourses);
+     res.json({
+        allCourses: allCourses,
+    });
 });
 
 module.exports = router;
